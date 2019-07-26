30a28dc72bb230d8ff0135620af573bad0d310fd
Yeri <arifyeripratama@gmail.com>
2019-07-26 21:27:19 +0700

    Updates

---
+++ precache-manifest.5122e5754853064bde41fd6e2a82bfd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7ca8d5b3f312365e89386ff2d4ccbc",
+    "url": "/alokasi/index.html"
+  },
+  {
+    "revision": "a3a2660d014630aae47f",
+    "url": "/alokasi/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "924d0298d3b0ad8ad2a6",
+    "url": "/alokasi/static/css/main.abc26ff8.chunk.css"
+  },
+  {
+    "revision": "a3a2660d014630aae47f",
+    "url": "/alokasi/static/js/2.bddf95ef.chunk.js"
+  },
+  {
+    "revision": "924d0298d3b0ad8ad2a6",
+    "url": "/alokasi/static/js/main.5437d95b.chunk.js"
+  },
+  {
+    "revision": "c5b5fe7d5860674b114b",
+    "url": "/alokasi/static/js/runtime~main.78ba4bcf.js"
+  }
+]);
